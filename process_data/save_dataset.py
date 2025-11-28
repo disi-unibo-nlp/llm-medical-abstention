ce35bf8f0173bfe64403b6197b8ed13c584b62ef
Alessio Cocchieri <alessio.cocchieri@outlook.it>
2025-11-28 18:01:16 +0100

    add code to save processed datasets

---
+++ process_data/save_dataset.py
@@ -0,0 +1,41 @@
+import json
+
+BENCHMARK = "medmcqa"
+input_file = f"out/classification/gemini_api/gemini-2.5-flash/medmcqa/2025-11-25_10-24-35/classification_medmcqa.jsonl"
+
+if BENCHMARK in ["medqa_4opt", "medqa_5opt", "medmcqa"]:
+    with open(f'data/bench/{BENCHMARK}.jsonl') as f:
+        data_bench = [json.loads(line) for line in f.readlines()]
+        id2item = {d['id']: d for d in data_bench}
+else:  # medxpertqa
+    from datasets import load_dataset
+    data_bench = load_dataset('TsinghuaC3I/MedXpertQA', "Text", split='test')
+    id2item = {d['id']: d for d in data_bench}
+
+with open(input_file) as f:
+    data_classified = [json.loads(line) for line in f.readlines()]
+    print(data_classified[0]['final_answer'])
+idx_LT = [d['id_question'] for d in data_classified if d['final_answer']['label'] == "LT"]
+idx_S = [d['id_question'] for d in data_classified if d['final_answer']['label'] == "S"]
+
+file_name = input_file.split("/")[-1]
+
+bench_LT = [id2item[idx] for idx in idx_LT]
+bench_S = [id2item[idx] for idx in idx_S]
+
+print(f"Total LT: {len(bench_LT)}")
+print(f"Total S: {len(bench_S)}")
+print(f"Total: {len(bench_LT) + len(bench_S)}")
+print(f"Original Benchmark Size: {len(data_bench)}")
+
+import os
+os.makedirs(f'data/bench/{BENCHMARK}', exist_ok=True)
+with open(f'data/bench/{BENCHMARK}/{BENCHMARK}_LT.jsonl', 'w') as f:
+    for item in bench_LT:
+        json.dump(item, f, ensure_ascii=False)
+        f.write("\n")
+
+with open(f'data/bench/{BENCHMARK}/{BENCHMARK}_S.jsonl', 'w') as f:
+    for item in bench_S:
+        json.dump(item, f, ensure_ascii=False)
+        f.write("\n")
