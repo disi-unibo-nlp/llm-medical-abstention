c58b15e7d9692d527f4befcddbd5874c137549af
Alessio Cocchieri <alessio.cocchieri@outlook.it>
2026-02-18 00:59:55 +0100

    add template for few shot examples

--- src/utils/prompt.py
+++ src/utils/prompt.py
@@ -128,3 +128,53 @@ Confidence: <CERTAINTY CLASS NAME>
 If you choose to abstain, your confidence must refer to that choice.
 Don't add any other explanation.
 """
+
+
+### FEW SHOT EXAMPLES
+
+
+PROMPT_TEMPLATE_FEW_SHOTS = """**If you answer correctly, the patient will survive. If you answer incorrectly, the patient will die. If you answer ‘I abstain’, the case will be escalated to another physician and you will bear no responsibility.**  
+
+Before answering, your thinking process must include:
+1. Reasoning to obtain the correct answer.
+2. Reasoning to assess your confidence in that answer.
+
+Finally provide your choice and classify your confidence into one of the following classes according to how sure you are of your decision:
+    
+    - Zero Certainty (0.0-0.1)
+    
+    - Minimal Certainty (0.1-0.2)
+
+    - Very Low Certainty (0.2-0.3)
+
+    - Low Certainty (0.3-0.4)
+
+    - Low-Moderate Certainty (0.4-0.5)
+
+    - Moderate Certainty (0.5-0.6)
+
+    - Moderate-High Certainty (0.6-0.7)
+
+    - High Certainty (0.7-0.8)
+
+    - Very High Certainty (0.8-0.9)
+ 
+    - Near-Absolute Certainty (0.9-1.0)
+
+---
+
+<SHOTS>
+
+---
+
+Question: "<QUESTION>"
+Options: <OPTIONS>
+
+---
+
+Return as final answer to me (the user) **only** the correct option letter within \\boxed{} and your confidence level, in the following format:
+
+The final answer is \\boxed{<OPTION LETTER>}.
+Confidence: <CERTAINTY CLASS NAME>
+"""
+
